9ff78504194172888911c55b8b1ca9173ded8d39
jobby <kyuhokim.11@gmail.com>
2025-11-03 15:49:15 +0900

    🔐 사용자 타입별 페이지 인증처리 구분
    비인증 사용자, 인증사용자, 관리자 사용자 별 접근 페이지 구분 및 정리

--- src/router/Approuter.tsx
+++ src/router/Approuter.tsx
@@ -18,27 +18,41 @@ import MyLecturePage from "../pages/Lecture/MyLecturePage";
 import LectureRoomPage from "../pages/Lecture/LectureRoomPage";
 
 
+
+// 사용자 타입별 페이지 인증 처리 예시
+/*
+* 1. 비인증 사용자: 로그인, 회원가입 페이지 (AuthLayout)
+* 2. 인증 사용자: 강의 목록, 강의 상세, 내 강의, 프로필 등 (MainLayout)
+* 3. 관리자 사용자: 관리자 대시보드, 강의 관리, 결제 관리, 사용자 관리 등 (MainLayout)
+*/
+
 export default function Approuter() {
     return (
         <BrowserRouter>
             <Routes>
+                {/* 비인증용 페이지 */}
                 <Route element={<AuthLayout/>}>
                     <Route path={ROUTES.LOGIN} element={<LoginPage/>} />
                     <Route path={ROUTES.SIGNUP} element={<SignUpPage/>} />
+                    {/* 인증 사용자용 페이지 */}
                 </Route>
                     <Route element={<MainLayout/>}>
                     <Route path={ROUTES.HOME} element={<ButtonGallery />} />
+
+                    {/* 로그인 여부에 따라 다르게 표시되는 페이지 */}
                     <Route path={ROUTES.LECTURE_LIST} element={<LectureListPage />} />  
                     <Route path={ROUTES.LECTURE_DETAIL} element={<LectureDetailPage />} />  
                     <Route path={ROUTES.LECTURE_PAYMENT} element={<LecturePaymentPage />} />
                     <Route path={ROUTES.LECTURE_PAYMENT_RESULT} element={<PaymentResultPage />} />
                     <Route path={ROUTES.MY_LECTURES} element={<MyLecturePage />} />
                     <Route path={ROUTES.LECTURE_ROOM} element={<LectureRoomPage />} />
+                    <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
+
+                    {/* 관리자용 페이지 개발 후에는 일바 */}
                     <Route path={ROUTES.ADMIN_DASHBOARD} element={<DashBoardPage />} />
                     <Route path={ROUTES.ADMIN_LECTURE_MANAGE} element={<LectureManagePage />} />
                     <Route path={ROUTES.ADMIN_PAYMENT_MANAGE} element={<PaymentManagePage />} />
                     <Route path={ROUTES.ADMIN_USER_MANAGE} element={<UserManagePage />} />
-                    <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
                 </Route>
             </Routes>
         </BrowserRouter>
