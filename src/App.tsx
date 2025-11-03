15a949693e32c0ce1feed35e6ed621ad45b365f9
chaehyeon <kch971109@naver.com>
2025-11-03 11:40:11 +0900

    ✨  feat(fe): App 컴포넌트에 InputGallery 반영
    InputGallery 컴포넌트 import 및 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/globalStyle';
 import theme from './styles/theme';
 import ButtonGallery from './pages/ButtonGallery';
+import InputGallery from './pages/InputGallery';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <ButtonGallery/>
+      <ButtonGallery />
+      <InputGallery />
     </ThemeProvider>
   );
 }
