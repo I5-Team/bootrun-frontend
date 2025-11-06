f73f7c4703c9e73372f67535bb75d85c83d7dac7
김민주 <891km@ewhain.net>
2025-11-07 02:33:42 +0900

    ✨  feat(fe): 필터 태그 일부 기능 추가

--- src/components/FilterForm.tsx
+++ src/components/FilterForm.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
 
+import Tag from "../components/Tag";
+import Button from "../components/Button";
+import SvgReset from "../assets/icons/icon-reset.svg?react";
+
 type FilterFormProps = {
     label: string,
     queryName: string,
@@ -11,6 +15,11 @@ type FilterFormProps = {
 type QueryParams = {
     [key:string]: string[];
 }
+
+type SelectedState = {
+    options: QueryParams;
+    names: string[];
+};
 
 const StyledFilterForm = styled.form`
     width: 100%;
@@ -78,62 +87,161 @@ const StyledFilterItem = styled.li`
     }
 `;
 
+const StyledTagWrapper = styled.div`
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    gap: 2rem;
+    margin-bottom: 5.2rem;
+`;
+
+const FilterTags = ({ selected, setSelected }: { 
+    selected: SelectedState, 
+    setSelected: React.Dispatch<React.SetStateAction<SelectedState>>, 
+}) => {
+    const handleReset = () => {
+        setSelected({ options: {}, names: [] });
+    };
+
+    const handleDelete = () => {
+        // 
+    };
+    
+    return (
+        <StyledTagWrapper>
+            <Button variant="outline" iconSvg={<SvgReset/>} onClick={handleReset}>
+                필터 초기화
+            </Button>
+            {selected.names.map(tagName => (
+                <button key={tagName} onClick={handleDelete}>
+                    <Tag variant="dark" hasDelete={true}>{tagName}</Tag>
+                </button>
+            ))}
+            
+        </StyledTagWrapper>
+    )
+};
+
 const FilterForm = ({ filterData }: { filterData: FilterFormProps[] }) => {
-    const [, setSearchParams] = useSearchParams();
-    const [selectedOptions, setSelectedOptions] = useState<QueryParams>({});
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [isInitialized, setIsInitialized] = useState(false);
+    // const [selectedOptions, setSelectedOptions] = useState<QueryParams>({});
+    // const [selectedTagNames, setSelectedTagNames] = useState<string[]>([]);
+    const [selected, setSelected] = useState<SelectedState>({ options: {}, names: [] });
+
+
+    useEffect(() => {
+        const initialOptions: QueryParams = {};
+        const initialNames: string[] = [];
+
+        for (const key of searchParams.keys()) {
+            const values = searchParams.getAll(key);
+            initialOptions[key] = values;
+
+            values.forEach(value => {
+            const field = filterData.find(f => f.queryName === key);
+            if (field) {
+                const option = field.options.find(o => o.value === value);
+                if (option) initialNames.push(option.label);
+            }
+            });
+        }
+
+        setSelected({ options: initialOptions, names: initialNames });
+        setIsInitialized(true);
+    }, []);
 
     const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value, checked} = e.target;
+        const {name, value, checked, dataset} = e.target;
+        const displayName = dataset.displayName;
+        if (!displayName) return;
 
-        setSelectedOptions(prev => {
-            const currentOptions = prev[name] || [];
+        setSelected(prev => {
+            const currentOptions = prev.options[name] || [];
             const newOptions = checked
                 ? [...currentOptions, value]
                 : currentOptions.filter(optValue => optValue !== value);
-            return {...prev, [name]: newOptions};
+
+            const newNames = checked
+            ? [...prev.names, displayName]
+            : prev.names.filter(name => name !== displayName);
+
+            return { options: { ...prev.options, [name]: newOptions }, names: newNames };
+            
         })
+
+        // setSelectedOptions(prev => {
+        //     const currentOptions = prev[name] || [];
+        //     const newOptions = checked
+        //         ? [...currentOptions, value]
+        //         : currentOptions.filter(optValue => optValue !== value);
+        //     return {...prev, [name]: newOptions};
+        // })
+
+        // setSelectedTagNames(prev => (
+        //     checked
+        //     ? [...prev, displayName]
+        //     : prev.filter(name => name !== displayName)
+        // ))
     }
 
-    useEffect(() => {
-        const params = new URLSearchParams();
+    // useEffect(() => {
+    //     if (!isInitialized) return;
 
-        Object.keys(selectedOptions).forEach(key => {
-            selectedOptions[key].forEach(value => {
-                params.append(key, value);
-            })
-        })
-        
+    //     const params = new URLSearchParams();
+
+    //     Object.keys(selectedOptions).forEach(key => {
+    //         selectedOptions[key].forEach(value => {
+    //             params.append(key, value);
+    //         })
+    //     })
+    //     setSearchParams(params);
+    // }, [selectedOptions, isInitialized]);
+
+    useEffect(() => {
+        if (!isInitialized) return;
+
+        const params = new URLSearchParams();
+        Object.keys(selected.options).forEach(key => {
+        selected.options[key].forEach(value => {
+            params.append(key, value);
+        });
+        });
         setSearchParams(params);
-    }, [selectedOptions]);
+    }, [selected, isInitialized, setSearchParams]);
 
     return (
-        <StyledFilterForm>
-            {filterData.map(item => (
-                <StyledFieldset key={item.label}>
-                    <legend className="sr-only">{item.label}</legend>
-                    <span>{item.label}</span>
+        <>
+            <StyledFilterForm>
+                {filterData.map(item => (
+                    <StyledFieldset key={item.label}>
+                        <legend className="sr-only">{item.label}</legend>
+                        <span>{item.label}</span>
 
-                    <StyledFilterList>
-                        {item.options.map((option) => (
-                            <StyledFilterItem key={option.value}>
-                                <label htmlFor={option.value}>
-                                    <input 
-                                        id={option.value} 
-                                        name={item.queryName}
-                                        value={option.value}
-                                        type="checkbox"
-                                        onChange={handleCheck}
-                                        checked={selectedOptions[item.queryName]?.includes(option.value) || false}
-                                        />
-                                    <span>{option.label}</span>
-                                </label>
-                            </StyledFilterItem>
-                        ))
-                        }
-                    </StyledFilterList>
-                </StyledFieldset>              
-            ))}
-        </StyledFilterForm>
+                        <StyledFilterList>
+                            {item.options.map((option) => (
+                                <StyledFilterItem key={option.value}>
+                                    <label htmlFor={option.value}>
+                                        <input
+                                            id={option.value} 
+                                            name={item.queryName}
+                                            value={option.value}
+                                            type="checkbox"
+                                            data-display-name={option.label}
+                                            onChange={handleCheck}
+                                            checked={selected.options[item.queryName]?.includes(option.value) || false}
+                                            />
+                                        <span>{option.label}</span>
+                                    </label>
+                                </StyledFilterItem>
+                            ))
+                            }
+                        </StyledFilterList>
+                    </StyledFieldset>              
+                ))}
+            </StyledFilterForm>
+            <FilterTags selected={selected} setSelected={setSelected}/>          
+    </>
     )
 }
 
