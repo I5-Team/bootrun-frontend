ad54217630b8b80505415c9a9c39b1603ac4960c
chaehyeon <kch971109@naver.com>
2025-11-14 18:32:18 +0900

    🔥  remove(fe): 불필요 코드 삭제

--- src/queries/useUserQueries.ts
+++ src/queries/useUserQueries.ts
@@ -1,6 +1,6 @@
 // 관리자 - 사용자 관리 훅 모음
 
-import { keepPreviousData, useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { activateUser, deactivateUser, fetchUsers, fetchUserDetail } from '../api/adminApi';
 import type { UserApiParams } from '../types/AdminUserType';
 
@@ -11,7 +11,6 @@ export const useUserListQuery = (params: UserApiParams) => {
   return useQuery({
     queryKey: ['users', params],
     queryFn: () => fetchUsers(params),
-    placeholderData: keepPreviousData,
   });
 };
 
