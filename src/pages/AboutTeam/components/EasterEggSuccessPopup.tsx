d9d6ffcd2b6f3c00ed9e89cb2873398fa937d554
chaehyeon <kch971109@naver.com>
2025-12-15 00:01:08 +0900

    ✨  feat(fe): 이스터에그 안내 팝업 컴포넌트 구현

---
+++ src/pages/AboutTeam/components/EasterEggSuccessPopup.tsx
@@ -0,0 +1,33 @@
+import { AnimatePresence } from 'framer-motion';
+import S from '../styles/EasterEggSuccessPopup.styled';
+
+interface EasterEggSuccessPopupProps {
+  show: boolean;
+}
+
+export default function EasterEggSuccessPopup({ show }: EasterEggSuccessPopupProps) {
+  return (
+    <AnimatePresence>
+      {show && (
+        <>
+          <S.PopupOverlay
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          />
+          <S.SuccessMessage
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0, opacity: 0 }}
+            transition={{ type: 'spring', stiffness: 300, damping: 20 }}
+          >
+            <S.SuccessMessageHighlightedText>이스터에그 해제!</S.SuccessMessageHighlightedText>
+            <br />
+            <span>개발자 소개에 숨겨진 스토리가 나타났어요.</span>
+          </S.SuccessMessage>
+        </>
+      )}
+    </AnimatePresence>
+  );
+}
